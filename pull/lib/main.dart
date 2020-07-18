38f38e385432f148172e9c924f73a7d27af4a5fd
Jang MoonIk <mooneegee@Mac-mini.local>
2020-07-18 19:39:02 +0900

    pull refresh push test

--- pull/lib/main.dart
+++ pull/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 
 void main() {
@@ -7,13 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,32 +32,47 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<String> entries = <String>['A', 'B', 'C'];
   final List<int> colorCodes = <int>[600, 500, 100];
   final _controller = ScrollController();
-  Color _color = Colors.white;
-  // bool _releaseStart = false;
-  // bool _releasing = false;
+  Color _color = Colors.green;
   bool _processing = false;
   double prevOffset = 0.0;
-  String message = '';
+  String message = 'start';
+  bool _loading = false;
 
   @override
   void initState() {
-    _controller.addListener(_scrollListener);
+    _controller.addListener(
+      _scrollListener,
+    );
     super.initState();
   }
 
-  void _incrementCounter(int count) {
-    setState(
+  _incrementCounter(int count) async {
+    setState(() {
+      _loading = true;
+      debugPrint('loading $_loading');
+    });
+
+    Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
       () {
         for (int i = 0; i < count; ++i) {
           entries.add('New ${entries.length.toString()}');
         }
+      },
+    ).then(
+      (value) {
+        setState(() {
+          _loading = false;
+          debugPrint('loading $_loading');
+        });
       },
     );
   }
 
   _scrollListener() {
     if (_controller.offset >= _controller.position.maxScrollExtent) {
-      debugPrint(_controller.offset.toString() + ' ' + prevOffset.toString());
       if (prevOffset > _controller.offset) {
         if (!_processing) {
           setState(
@@ -76,43 +89,11 @@ class _MyHomePageState extends State<MyHomePage> {
         setState(
           () {
             message = "reach the bottom";
-            _color = Colors.pinkAccent;
+            _color = Colors.greenAccent;
             _processing = false;
           },
         );
       }
-      // if (_controller.position.userScrollDirection == ScrollDirection.forward) {
-
-      // }
-      // if (_releasing) {
-      //   _color = Colors.cyan;
-      //   if (!_processing) {
-      //     _incrementCounter(1);
-      //     _processing = true;
-      //   }
-      // }
-      // if (!_controller.position.outOfRange) {
-      //   setState(() {
-      //     message = "reach the bottom";
-      //     _color = Colors.amber;
-      //     _processing = false;
-      //   });
-      // }
-    }
-    if (_controller.offset <= _controller.position.minScrollExtent) {
-      // setState(() {
-      //   if (_releasing) {
-      //     _color = Colors.cyan;
-      //     _incrementCounter(1);
-      //     _releasing = false;
-      //   }
-      // });
-      // if (!_controller.position.outOfRange) {
-      //   setState(() {
-      //     message = "reach the top";
-      //     _color = Colors.amber;
-      //   });
-      // }
     }
     prevOffset = _controller.offset;
   }
@@ -122,6 +103,24 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        bottom: _loading
+            ? PreferredSize(
+                child: LinearProgressIndicator(
+                  backgroundColor: Colors.deepOrangeAccent,
+                  minHeight: 2.0,
+                ),
+                preferredSize: Size(
+                  double.infinity,
+                  2.0,
+                ),
+              )
+            : PreferredSize(
+                child: Container(),
+                preferredSize: Size(
+                  double.infinity,
+                  2.0,
+                ),
+              ),
       ),
       body: NotificationListener<ScrollNotification>(
         child: Column(
@@ -150,93 +149,69 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
         onNotification: (notification) {
-          if (notification is ScrollStartNotification) {
-            debugPrint('scroll start');
-          } else if (notification is ScrollUpdateNotification) {
-            debugPrint('scroll update');
-          } else if (notification is ScrollEndNotification) {
-            debugPrint('scroll end');
-          }
-          // debugPrint(notification.toString());
           // if (notification is ScrollStartNotification) {
-          //   _releaseStart = true;
-          // }
-          // if (notification is ScrollUpdateNotification &&
-          //     null == notification.dragDetails) {
-          //   _releasing = true;
+          //   debugPrint('scroll start');
+          // } else if (notification is ScrollUpdateNotification) {
+          //   debugPrint('scroll update');
+          // } else if (notification is ScrollEndNotification) {
+          //   debugPrint('scroll end');
           // }
           return true;
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.amberAccent,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.hotel,
+              color: Colors.amberAccent,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.https,
+              color: Colors.amberAccent,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.inbox,
+              color: Colors.amberAccent,
+            ),
+            title: Text(''),
+          ),
+        ],
+      ),
+      bottomSheet: _loading
+          ? Container(
+            height: 2.0,
+              child: LinearProgressIndicator(
+                backgroundColor: Colors.deepOrangeAccent,
+              ),
+            )
+          : Container(
+              height: 0.0,
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _incrementCounter(1),
+        onPressed: () {
+          setState(
+            () {
+              entries.clear();
+            },
+          );
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
-      // body: Column(
-      //   children: [
-      //     Container(
-      //       height: 50.0,
-      //       color: _color,
-      //       child: Center(
-      //         child: Text(message),
-      //       ),
-      //     ),
-      //     Expanded(
-      //       // child: Listener(
-      //       //   onPointerUp: (event) {
-      //       //     debugPrint('onPointerUp');
-      //       //   },
-      //       //   child: ListView.builder(
-      //       //     controller: _controller,
-      //       //     physics: BouncingScrollPhysics(),
-      //       //     padding: const EdgeInsets.all(8),
-      //       //     itemCount: entries.length,
-      //       //     itemBuilder: (BuildContext context, int index) {
-      //       //       return _buildElement(context, index);
-      //       //     },
-      //       //   ),
-      //       // ),
-
-      //       child: GestureDetector(
-      //         onVerticalDragStart: (details) {
-      //           _pullEnd = false;
-      //           debugPrint('onVerticalDragStart false');
-      //         },
-      //         onVerticalDragEnd: (details) {
-      //           _pullEnd = true;
-      //           debugPrint('onVerticalDragStart true');
-      //         },
-      //         child: ListView.builder(
-      //           // controller: _controller,
-      //           physics: BouncingScrollPhysics(),
-      //           padding: const EdgeInsets.all(8),
-      //           itemCount: entries.length,
-      //           itemBuilder: (BuildContext context, int index) {
-      //             return _buildElement(context, index);
-      //           },
-      //         ),
-      //       ),
-      //     ),
-      //   ],
-      // ),
-      // onTap: () => debugPrint('onVerticalDragStart false'),
-      // onVerticalDragStart: (details) {
-      //   _pullEnd = false;
-      //   debugPrint('onVerticalDragStart false');
-      // },
-      // onVerticalDragEnd: (details) {
-      //   _pullEnd = true;
-      //   debugPrint('onVerticalDragStart true');
-      // },
-      // onVerticalDragUpdate: (details) {
-      //   debugPrint('onVerticalDragUpdate');
-      // },
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () => _incrementCounter(1),
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
